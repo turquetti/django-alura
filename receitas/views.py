f3bb741e05067beb51b58ae729de9ba64fac9419
turquetti <gabrielaturquetti@gmail.com>
2021-10-19 23:31:25 -0300

    adiciona bd

--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
+    receitas = {
+        1:'Lasanha',
+        2:'Sopa de legumes',
+        3:'Sorvete',
+    }
+    
+    dados = {
+        'nome_das_receitas' :
+    }
     return render(request, 'index.html')
 
 def receita(request):
